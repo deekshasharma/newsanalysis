18c326e0ce5b0f39fa2a2ed0037b8615c8dd4571
Harit Himanshu <harit.himanshu@yahoo.in>
2013-05-26 14:50:17 -0700

    openshift support

--- app.py
+++ app.py
@@ -26,22 +26,14 @@ def run_simple_httpd_server(app, ip, port=8080):
 #
 # IMPORTANT: Put any additional includes below this line.  If placed above this
 # line, it's possible required libraries won't be in your searchable path
-# 
+#
 
 
 #
 #  main():
 #
 if __name__ == '__main__':
-   ip   = os.environ['OPENSHIFT_INTERNAL_IP']
-   port = 8080
-   zapp = imp.load_source('application', 'wsgi/application')
-
-   #  Use gevent if we have it, otherwise run a simple httpd server.
-   print 'Starting WSGIServer on %s:%d ... ' % (ip, port)
-   try:
-      run_gevent_server(zapp.application, ip, port)
-   except:
-      print 'gevent probably not installed - using default simple server ...'
-      run_simple_httpd_server(zapp.application, ip, port)
-
+    ip   = os.environ['OPENSHIFT_INTERNAL_IP']
+    port = 8080
+    from runserver import run
+    run(ip, port)
--- runserver.py
+++ runserver.py
@@ -1,2 +1,6 @@
-from application import app
-app.run(debug = True)
+def run(host, port):
+    from application import app
+    app.run(debug=True)
+
+if __name__ == '__main__':
+    run(host='localhost', port=5000)
